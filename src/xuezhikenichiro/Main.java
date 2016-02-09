c491db040fcc65ed266cdf16762410649080f76f
ArJaaay <jay-1994@hotmail.com>
2016-02-09 16:43:21 +0000

    camera class added

--- src/xuezhikenichiro/Main.java
+++ src/xuezhikenichiro/Main.java
@@ -5,24 +5,23 @@ import java.awt.*;
 
 public class Main {
 	public static void main(String[] args) {
-		
+
+		int i;
+
 		JFrame myFrame = new JFrame("");
-		myFrame.setSize(300,80);
-		
-		JSlider mySlider = new JSlider();
+		myFrame.setSize(500,500);
+
+		Camera myCamera = new Camera();
+
 		
 		
 		myFrame.setLayout(new BorderLayout());
-		JPanel jTop = new JPanel();
-		myFrame.add(jTop, BorderLayout.CENTER);
-		jTop.add(mySlider);
-
-		
-// test 
+		myFrame.add(myCamera, BorderLayout.NORTH);
 		
 		
 		
 		myFrame.setVisible(true);
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 }
