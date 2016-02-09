020e9affaa546f5c9bcc51f6779d3f41e79b7a9e
ArJaaay <jay-1994@hotmail.com>
2016-02-09 14:33:44 +0000

    no needed code

--- src/xuezhikenichiro/Main.java
+++ src/xuezhikenichiro/Main.java
@@ -10,26 +10,13 @@ public class Main {
 		myFrame.setSize(300,80);
 		
 		JSlider mySlider = new JSlider();
-		JLabel y = new JLabel("Yes");
-		JLabel n = new JLabel("No");
 		
 		
 		myFrame.setLayout(new BorderLayout());
 		JPanel jTop = new JPanel();
 		myFrame.add(jTop, BorderLayout.CENTER);
 		jTop.add(mySlider);
-		
-		JPanel jLeft = new JPanel();
-		myFrame.add(jLeft, BorderLayout.WEST);
-		jLeft.add(y);
-		
-		
-		
-		JPanel jRight = new JPanel();
-		myFrame.add(jRight, BorderLayout.EAST);
-		jRight.add(n);
-		
-		
+
 		
 // test 
 		
